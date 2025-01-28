390b277838943cf97aadc8254f521c0a9ba06f37
solena8 <solena_ananda@hotmail.com>
2025-01-28 13:51:35 +0100

    suite conway ameliorations

--- src/conway.py
+++ src/conway.py
@@ -6,24 +6,24 @@ class Conway:
         self.given_string: str = given_string
         self.nb_of_conway_loop: int = nb_of_conway_loop
         # string to add each line describing the previous
-        self.new_line = ''
+        self.new_line = ""
         # int counting how many times a single letter or number is repeated
-        self.conway_result = ''
+        self.conway_result = ""
 
 
     def conway_suite(self):
         # add given string for reference
         self.conway_result += self.given_string
         # iterating the given number of times
-        while self.nb_of_conway_loop > 0:
+        for _ in range(self.nb_of_conway_loop):
             # using groupby to split the given string into groups of similar caracters in a list
             splitted_string = ["".join(group) for ele, group in groupby(self.given_string)]
             # looping over the list
-            for i in range(len(splitted_string)):
+            for splitted_string_element in splitted_string:
                 # getting the caracter we're currenttly inspecting
-                caracter = splitted_string[i][0]
+                caracter = splitted_string_element[0]
                 # getting the nb of times it is repeated
-                nb_occurences = len(splitted_string[i])
+                nb_occurences = len(splitted_string_element)
                 # turning the nb into a string
                 str_occurences = str(nb_occurences)
                 # adding the nb of times + the caracter to the new line
@@ -36,12 +36,20 @@ class Conway:
             self.given_string = self.new_line
             # resetting new line to an empty string
             self.new_line = ""
-            # subtract 1 from the loop count
-            self.nb_of_conway_loop -= 1
         return self.conway_result
 
-test = Conway("1aaa",3)
-print(test.conway_suite())
+# test = Conway("1aaa",3)
+# print(test.conway_suite())
+#
+# test2 = Conway("b",7)
+# print(test2.conway_suite())
 
-test2 = Conway("b",7)
-print(test2.conway_suite())
+test3 = Conway("1aaa2a223bb2b",2)
+print(test3.conway_suite())
+
+# faire une methode pour la boucle for
+# faire une méthode print qui utilise les \n
+# faire un tableau pour le résultat du coup
+# splitter en objet avec un objet qui contient le caractere et le nb de recurence
+# récursif ?
+# map ou imap de itertools - prend en premier paarmetre une fonction et e dexuieme un tableau pour remplacer le for range
